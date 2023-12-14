ad1db73007bd06601960b03a44bef4ac97d3873e
STDG <stdg@novonordisk.com>
2023-12-14 11:35:38 +0100

    feat: cleaned up day 14

--- aoc_14/reflector_2.py
+++ aoc_14/reflector_2.py
@@ -25,36 +25,58 @@ def move_rock(x, y, data):
     return data
 
 
+def run_cycle(data):
+    (len_y, len_x) = np.shape(data)
+    for _ in ['N', 'W', 'S', 'E']:
+        for y in range(len_y):
+            for x in range(len_x):
+                if data[y][x] == "O":
+                    data = move_rock(x, y, data)
+        data = np.rot90(data, 3)
+    return data
+
+
 if __name__ == '__main__':
-    # At some point there is a periodicity after 44 cycles the data is the same again
-    # Cycle 1.000.000.000 should be the same as cycle 1.000.000.000 - 44 * n
-    # E.g. cycle 32, 76, 120, 164
+    # Find the periodicity (number of cycles after which the output is the same
+    # again). Cycle 1.000.000.000 should be the same as cycle
+    # 1.000.000.000 - 44 * n as long as the cycle is larger than the max size of
+    # the data --> In this specific case cycle 120 is the first where the cycle
+    # repeats and which is a multiple of 44 away from 1e9
+
+    rep = 1e9
+
     with open("input.txt") as f:
         data = f.read().splitlines()
     data = np.array([list(line) for line in data])
+
+    start_data = data.copy()
+
     (len_y, len_x) = np.shape(data)
 
-    history = []
-    history.append(data.copy())
-
+    # Find the periodicity in the cycles by comparing the output of cycle
+    # len(data) with the output of the following cycles, if it's the same we
+    # have a periodicity
     cycle = False
+    n = 0
+    compare_data = None
+    while not cycle:
+        n += 1
+        data = run_cycle(data)
+        if n == np.max(np.shape(data)):
+            compare_data = data.copy()
+        elif n >= np.max(np.shape(data)) and np.array_equal(data, compare_data):
+            p = n - np.max(np.shape(data))
+            print(f"Cycle {n} is the same as cycle {np.max(np.shape(data))}. Periodicity found p = {p}.")
+            cycle = True
 
-    # while not cycle:
-    for cycle in tqdm(range(1, 1000)):
-        for i in enumerate(['N', 'W', 'S', 'E']):
-            for y in range(len_y):
-                for x in range(len_x):
-                    if data[y][x] == "O":
-                        data = move_rock(x, y, data)
-            data = np.rot90(data, 3)
-        if cycle >= 164 and (cycle-164) % 44 == 0:
-            print(f"Cycle {cycle} is done: Total load {total_load(data)}")
-        # print(data, "\n")
-        # for h, data_h in enumerate(history):
-        #     if np.array_equal(data, data_h):
-        #         print(data)
-        #         print(data_h)
-        #         print(f"Data of iteration {n} is equal to historical data at iteration {h}")
-        #         # cycle = True
-        # else:
-        #     history.append(data.copy())
+    # Get the first cycle that has the same output as cycle 1e9
+    min_cycle = int(rep % p)
+    while min_cycle < np.max(np.shape(data)):
+        min_cycle += p
+
+    # Reset data to the initial state
+    data = start_data.copy()
+    # Run the cycles
+    for cycle in tqdm(range(0, min_cycle)):
+        data = run_cycle(data)
+    print(total_load(data))
